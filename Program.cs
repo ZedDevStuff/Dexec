0fb425b82509cfe7cbfc46c47615dad3f58b865b
ZedDevStuff <98429642+ZedDevStuff@users.noreply.github.com>
2025-08-23 08:11:28 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+namespace Dexec;
+
+internal partial class Program
+{
+    [GeneratedRegex(@"#(?:\s+)?(?<binary>.+)")]
+    public static partial Regex HeaderRegex();
+    [GeneratedRegex(@"\(env:(?<env_var>.+)\)")]
+    public static partial Regex VarRegex();
+    static void Main(string[] args)
+    {
+        if (args.Length == 0) return;
+        if (args[0] == "-s" || args[0] == "--setup")
+        {
+            Setup();
+            return;
+        }
+        if (!File.Exists(args[0])) return;
+        string? firstLine = File.ReadLines(args[0]).FirstOrDefault();
+        if(firstLine == null) return;
+        Match headerMatch = HeaderRegex().Match(firstLine);
+        if(!headerMatch.Success) return;
+        firstLine = headerMatch.Groups["binary"].Value;
+        MatchCollection envVars = VarRegex().Matches(firstLine);
+        foreach (Match match in envVars)
+        {
+            var value = Environment.GetEnvironmentVariable(match.Groups["env_var"].Value);
+            value ??= Environment.GetEnvironmentVariable(match.Groups["env_var"].Value, EnvironmentVariableTarget.User);
+            value ??= Environment.GetEnvironmentVariable(match.Groups["env_var"].Value, EnvironmentVariableTarget.Machine);
+            if (value is null) return;
+            firstLine = firstLine.Replace(match.Value, value);
+        }
+        Process process = new Process();
+        string[] split = firstLine.Split(' ');
+        if (split.Length > 1)
+            process.StartInfo = new ProcessStartInfo(split[0], string.Join(' ', [.. split[1..], args[0]]));
+        else
+            process.StartInfo = new ProcessStartInfo(firstLine, args[0]);
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+    }
+
+    [SupportedOSPlatform("Windows")]
+    static void Setup()
+    {
+        // Register self in window's context menu
+        RegistryKey? key = Registry.ClassesRoot.OpenSubKey(@"*\shell");
+        if(key != null)
+        {
+            var localRoot = key.CreateSubKey("Dexec");
+            localRoot.SetValue("", "Dexec");
+            localRoot.CreateSubKey("command").SetValue("", $"\"{Path.Combine(AppContext.BaseDirectory, "Dexec.exe")}\" \"%1\"");
+        }
+    }
+}
